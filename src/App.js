4bfbeba8aed78331d1599711872ff2e711f0e814
Divyam4745 <divyamvishwakarma34@gmail.com>
2026-07-15 11:55:29 +0530

    FormValidation  in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import './App.css';
-import About from './component/About';
+// import About from './component/About';
 // import TextForms from './component/TextForms';
+import FormExample from './component/ForExample';
 
 function App() {
-  return (<>
-    <div>
+  return (
+   <div>
     {/* <div className="container my-3"></div> */}
     {/* <TextForms heading="Enter the text analyze below"/> */}
-     <About/>
-    </div>
-    </>
+     {/* <About/> */}
+     <h1></h1>
+     <FormExample/>
+   </div>
   );
 }
 export default App
